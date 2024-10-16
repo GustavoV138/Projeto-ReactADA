b19c232effa53b15a93878fe58f5e16c2cd7239f
Gustavo Vieira <joao.g138+github@gmail.com>
2024-10-16 18:02:34 -0300

    1

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+import logoImg from '../../assets/images/logo.avif';
+
+import './styles.css'
+
+class Navbar extends React.Component {
+    render() {
+        return(
+            <header>
+
+                <nav id="navbar">
+                <div className="nav-brand">
+                    <img src={logoImg} alt="Logo da página" width="60px"/>
+                    <h1>Space Flight News</h1>
+                </div>
+
+                <ul className="nav-list">
+                    <li>
+                        <a href="/" id="home">Home</a>
+                    </li>
+                    <li>
+                        <a href="/">Trending</a>
+                    </li>
+                    <li>
+                        <a href="/">Categories</a>
+                    </li>
+                    <li>
+                        <a href="/">About us</a>
+                    </li>
+                </ul>
+                </nav>
+            </header>
+        )
+    }
+}
+
+export default Navbar;
